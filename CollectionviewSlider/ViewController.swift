66ee1a5039ec4b9c0a1d47513bc7a13657e13ce7
akinsete <sundaygabrielakinsete@gmail.com>
2017-02-12 12:31:24 +0100

    Initial Commit

---
+++ CollectionviewSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionviewSlider
+//
+//  Created by Akinsete Sunday on 2/12/17.
+//  Copyright © 2017 appsng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
